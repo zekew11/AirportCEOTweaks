7c85e73f67fb005bf6ba11eb94d1afafb0a1a988
zekew11 <zekelukew@gmail.com>
2024-07-12 20:44:05 -0700

    init planningreplanned

---
+++ AirportCEOPlanningReplanned/ModLoaderInteractionHandler.cs
@@ -0,0 +1,24 @@
+﻿using AirportCEOModLoader.WatermarkUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AirportCEOPlanningReplanned
+{
+
+    internal class ModLoaderInteractionHandler
+    {
+        internal static void SetUpInteractions()
+        {
+            // More will probably be added!
+            AirportCEOPlanningReplanned.LogInfo("Seting up ModLoader interactions");
+
+            WatermarkUtils.Register(new WatermarkInfo("T-PR", Assembly.GetExecutingAssembly().GetName().Version.ToString(), true));
+
+            AirportCEOPlanningReplanned.LogInfo("Completed ModLoader interactions!");
+        }
+    }
+}
